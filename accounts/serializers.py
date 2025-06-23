916ff88cba267a3264bbd727b32cfc053f2662b5
tiziani <lorenzo.tiziani04@gmail.com>
2025-06-23 17:55:35 +0200

    firstCommit

---
+++ accounts/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'first_name', 'last_name')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+        )
+        return user
+
+# NUOVO SERIALIZZATORE PER PERSONALIZZARE IL PAYLOAD JWT
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['email'] = user.email
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['is_2fa_enabled'] = user.is_2fa_enabled
+
+        return token
